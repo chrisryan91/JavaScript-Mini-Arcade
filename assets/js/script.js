0163460b7036702b8974aacf3ce61399c8ce9236
Chris <christopherryan91@gmail.com>
2023-07-16 18:10:51 +0100

    create getResult2 function to determine who the winner is - then display result in result2display

--- assets/js/script.js
+++ assets/js/script.js
@@ -126,7 +126,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const computerChoiceDisplay = document.getElementById('computer-chooses')
     const userChoiceDisplay = document.getElementById('you-choose')
-    const resultDisplay = document.getElementById('result2')
+    const result2Display = document.getElementById('result2')
     const possibleChoices = document.querySelectorAll('button')
     let userChoice
     let result2
@@ -134,7 +134,8 @@ document.addEventListener('DOMContentLoaded', () => {
     possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
         userChoice = e.target.id
         userChoiceDisplay.innerHTML = userChoice
-    
+        generateComputerChoice()
+        getResult2()
     }))
 
     function generateComputerChoice() {
@@ -151,6 +152,39 @@ document.addEventListener('DOMContentLoaded', () => {
         }
         
         computerChoiceDisplay.innerHTML = computerChoice
+    }
+
+    function getResult2() {
+        if (computerChoice === userChoice) {
+            result2 = 'Draw!'
+        }
+    
+        if (computerChoice === 'rock' && userChoice === "paper") {
+            result2 = 'You win!'
+        }
+    
+        if (computerChoice === 'scissors' && userChoice === "paper") {
+            result2 = 'You lose!'
+        }
+    
+        if (computerChoice === 'paper' && userChoice === "rock"){
+            result2 = 'You lose!'
+        }
+    
+        if (computerChoice === 'rock' && userChoice === "scissors") {
+            result2 = 'You lose!'
+        }
+    
+        if (computerChoice === 'paper' && userChoice === "scissors"){
+            result2 = 'You win!'
+        }
+    
+        if (computerChoice === 'scissors' && userChoice === "rock"){
+            result2 = 'You win!'
+        }
+
+        result2Display.innerHTML = result2
+
     }
 
     })

e4c15ea37bd81f49f7728859b6f10a08cb381b34
Chris <christopherryan91@gmail.com>
2023-07-16 17:05:10 +0100

    add checkBoard() function to check if the created arrays are equal to the winning arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,37 @@ document.addEventListener('DOMContentLoaded', () => {
     const result = document.querySelector('#result')
     const displayCurrentPlayer = document.querySelector('#current-player')
     let currentPlayer = 1
+
+    function checkBoard() {
+        for (let y = 0; y < winningArrays.length; y++) {
+          const square1 = squares[winningArrays[y][0]]
+          const square2 = squares[winningArrays[y][1]]
+          const square3 = squares[winningArrays[y][2]]
+          const square4 = squares[winningArrays[y][3]]
+    
+          if (
+            square1.classList.contains('player-one') &&
+            square2.classList.contains('player-one') &&
+            square3.classList.contains('player-one') &&
+            square4.classList.contains('player-one')
+          )
+          {
+            result.innerHTML = 'Player One Wins!'
+          }
+          if (
+            square1.classList.contains('player-two') &&
+            square2.classList.contains('player-two') &&
+            square3.classList.contains('player-two') &&
+            square4.classList.contains('player-two')
+          )
+          {
+            result.innerHTML = 'Player Two Wins!'
+          }
+        }
+      }
   
     for (let i = 0; i < squares.length; i++) {
         squares[i].onclick = () => {
-          //if the square below your current square is taken, you can go ontop of it
           if (squares[i + 7].classList.contains('taken') &&!squares[i].classList.contains('taken')) {
             if (currentPlayer == 1) {
               squares[i].classList.add('taken')
@@ -24,6 +51,4 @@ document.addEventListener('DOMContentLoaded', () => {
           checkBoard()
         }
       }
-
-
   })

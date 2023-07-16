b271fa23eb8e30951b2ff23c8a139f26a31653ea
Chris <christopherryan91@gmail.com>
2023-07-16 23:09:35 +0100

    add variables for the snake itself at the start of the game, the speed at the beginning (0), an empty area which will serve as the snakes body and the variable for the food which will appear on the canvas

--- assets/js/snake.js
+++ assets/js/snake.js
@@ -1,12 +1,28 @@
 var blockSize = 25;
 var rows = 20;
-var cols = 60;
+var cols = 20;
 var board;
 var context; 
+
+var snakeX = blockSize * 5;
+var snakeY = blockSize * 5;
+
+var velocityX = 0; 
+var velocityY = 0;
+
+var snakeBody = [];
+
+var foodX;
+var foodY;
 
 window.onload = function() {
     board = document.getElementById("board");
     board.height = rows * blockSize;
     board.width = cols * blockSize;
     context = board.getContext("2d");
+
+    placeFood();
+    document.addEventListener("keyup", changeDirection);
+    // update();
+    setInterval(update, 1000/10);
 }

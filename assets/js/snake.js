42546aacfb84d26e03601ee8a62551b88f601642
Chris <christopherryan91@gmail.com>
2023-07-17 22:10:24 +0100

    add variables for snake body and added square to board using fillStyle

--- assets/js/snake.js
+++ assets/js/snake.js
@@ -7,31 +7,19 @@ var context;
 var snakeX = blockSize * 5;
 var snakeY = blockSize * 5;
 
-var velocityX = 0; 
-var velocityY = 0;
-
-var snakeBody = [];
-
-var foodX;
-var foodY;
-
 window.onload = function() {
     board = document.getElementById("board");
     board.height = rows * blockSize;
     board.width = cols * blockSize;
     context = board.getContext("2d");
 
-    placeFood();
-    document.addEventListener("keyup", changeDirection);
-    // update();
-    setInterval(update, 1000/10);
+    update() 
 }
 
 function update() {
-
-    context.fillStyle="#000";
+    context.fillStyle="black";
     context.fillRect(0, 0, board.width, board.height);
 
-    context.fillStyle="white";
-    context.fillRect(foodX, foodY, blockSize, blockSize);
+    context.fillStyle = "red";
+    context.fillRect(snakeX, snakey, blocksize, blocksize);
 }

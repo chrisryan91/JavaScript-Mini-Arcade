2443028490a1c922a2940df76534159e2f77c65d
Chris <christopherryan91@gmail.com>
2023-07-18 19:14:22 +0100

    save hitPosition as the id of the random square. declare hitPosition. add eventListener for each square in array to listen for mousedown. if id of clicked square is equal to id of randomSquare (hitPosition) then get result and add to result. display result in score

--- assets/js/clickgame.js
+++ assets/js/clickgame.js
@@ -4,20 +4,31 @@ const timeLeft = document.querySelector('#remaining-time');
 const score = document.querySelector('#score');
 
 let result = 0;
+let hitPosition
 
 function randomSquare() {
     squares.forEach(square => {
         square.classList.remove('click')
     })
 
-    let randomSquare = squares[Math.floor(Math.random * 9)]
+    let randomSquare = squares[Math.floor(Math.random() * 9)]
     randomSquare.classList.add('click')
 
 }
 
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if (square.id == hitPosition) {
+            result++
+            score.textContent = result
+            hitPosition = null
+        }
+    })
+})
+
 function moveMole() {
     let timerId = null
-    timeId = setInterval(randomSquare, 500)
+    timerId = setInterval(randomSquare, 500)
 }
 
 moveMole();

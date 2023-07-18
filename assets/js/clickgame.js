89afe2bf3d0c588097eb51db26a64ff775012f3e
Chris <christopherryan91@gmail.com>
2023-07-18 18:39:35 +0100

    add variables click, squares, timeLeft and score

---
+++ assets/js/clickgame.js
@@ -0,0 +1,5 @@
+const squares = document.querySelectorAll('.square')
+const click = document.querySelector('.click')
+const timeLeft = document.querySelector('#time-left')
+const score = document.querySelector('#score')
+

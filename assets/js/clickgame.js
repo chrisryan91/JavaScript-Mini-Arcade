abcfa00ffd26aaeeb3f4a4bfcec82408747e0f85
Chris <christopherryan91@gmail.com>
2023-07-18 19:03:14 +0100

    create moveMole function with timerId variable and the timerId set with an interval

--- assets/js/clickgame.js
+++ assets/js/clickgame.js
@@ -1,5 +1,23 @@
-const squares = document.querySelectorAll('.square')
-const click = document.querySelector('.click')
-const timeLeft = document.querySelector('#time-left')
-const score = document.querySelector('#score')
+const squares = document.querySelectorAll('.square');
+const click = document.querySelector('.click');
+const timeLeft = document.querySelector('#remaining-time');
+const score = document.querySelector('#score');
 
+let result = 0;
+
+function randomSquare() {
+    squares.forEach(square => {
+        square.classList.remove('click')
+    })
+
+    let randomSquare = squares[Math.floor(Math.random * 9)]
+    randomSquare.classList.add('click')
+
+}
+
+function moveMole() {
+    let timerId = null
+    timeId = setInterval(randomSquare, 500)
+}
+
+moveMole();

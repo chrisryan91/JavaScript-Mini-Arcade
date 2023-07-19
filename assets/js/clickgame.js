d9a63c9a3a6657856c689b8b74de337a7224470a
Chris <christopherryan91@gmail.com>
2023-07-19 21:54:42 +0100

    add restart button below clickgame

--- assets/js/clickgame.js
+++ assets/js/clickgame.js
@@ -5,7 +5,7 @@ const score = document.querySelector('#score');
 
 let result = 0;
 let hitPosition;
-let currentTime = 60
+let currentTime = 60;
 
 function randomSquare() {
     squares.forEach(square => {
@@ -41,9 +41,16 @@ function countDown () {
     timeLeft.textContent = currentTime;
 
     if (currentTime === 0) {
-        clearInterval(countDownTimerId)
-        alert('Game over! Your score is' + result)
+       clearInterval(countDownTimerId)
+       alert('Game over! Your score is' + result)
     }
 }
 
 let countDownTimerId = setInterval(countDown, 1000)
+
+var restart = document.getElementById('restart');
+
+document.querySelector('.restart-btn').addEventListener('click', function(){
+    window.location.reload();
+    return false;
+  });

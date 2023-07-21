3230e14fcc3cc4e073f8506b60db8fac4e2a5a26
Chris <christopherryan91@gmail.com>
2023-07-21 23:42:23 +0100

    fix bug when eventListener was not called - fix id tags

--- assets/js/rockpaperscissors.js
+++ assets/js/rockpaperscissors.js
@@ -1,78 +1,80 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    let userScore = 0;
-    let computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
+
+const userScoreSpan = document.getElementById("user-result");
+const computerScoreSpan = document.getElementById("computer-result");
     
-    const scoreBoard_div = document.querySelector(".score-board");
-    const winner_div = document.querySelector(".winner > p");
-    const result2Display = document.getElementById('result2');
-    const possibleChoices = document.querySelectorAll('button');
+const scoreBoard_div = document.querySelector(".score-board");
+const winner_div = document.querySelector(".winner > p");
 
-    const rck_div = document.getElementById("r");
-    const ppr_div = document.getElementById("p");
-    const scssr_div = document.getElementById("s");
+const rck_div = document.getElementById("Rock");
+const ppr_div = document.getElementById("Paper");
+const scssr_div = document.getElementById("Scissor");
 
-    function getComputerChoice() {
-        const choices = ['r', 'p', 's'];
-        const randomNumber = Math.floor(Math.random()*3);
-        return choices[randomNumber];
-    }
+function getComputerChoice() {
+const choices = ['Rock', 'Paper', 'Scissor'];
+const randomNumber = Math.floor(Math.random()*3);
+return choices[randomNumber];
+}
 
-    function win(userChoice, computerChoice) {
-        userScore++;
-        userScoreSpan.innerHTML = userScore;
-        computerScoreSpan.innerHTML = computerScore;
-        winner_div.innerHTML = convertToWord(userChoice) + " beats " + convertToWord(computerChoice) + "!";
-        }
-    
-    function lose(userChoice, computerChoice) {
-        computerScore++;
-        computerScoreSpan.innerHTML = computerScore;
-        userScoreSpan.innerHTML = userScore;
-        winner_div.innerHTML = convertToWord(computerChoice) + " beats " + convertToWord(userChoice) + "!";
-    }
-    
-    function draw(userChoice, computerChoice) {
-        computerScoreSpan.innerHTML = computerScore;
-        userScoreSpan.innerHTML = userScore;
-        winner_div.textContent = "Draw!";
-    }
+function win(userChoice, computerChoice) {
+userScore++;
+userScoreSpan.innerHTML = userScore;
+computerScoreSpan.innerHTML = computerScore;
+winner_div.innerHTML = userChoice + " beats " + computerChoice + "!";
+}
 
-    function game(userChoice) {
-        const computerChoice = getComputerChoice();
-        switch (userChoice + computerChoice){
-            case "rs":
-            case "pr":
-            case "sp":
-                    win(userChoice, computerChoice);
-                    break;
-            case "rp":
-            case "ps":
-            case "sr": 
-                    lose(userChoice, computerChoice);
-                    break;
-            case "rr":
-            case "pp":
-            case "ss":
-                    draw(userChoice, computerChoice);
-                    break;
-        }
-    }
+function lose(userChoice, computerChoice) {
+computerScore++;
+computerScoreSpan.innerHTML = computerScore;
+userScoreSpan.innerHTML = userScore;
+winner_div.innerHTML = computerChoice + " beats " + userChoice + "!";
+}
 
-    function main() {
-        ppr_div.addEventListener('click', function() {
-            game("p");
-        })
-        
-        rck_div.addEventListener('click', function() {
-            game("r");
-        })
-        
-        scssr_div.addEventListener('click', function() {
-            game("s");
-        })
-        
-        }
-    })
+function draw(userChoice, computerChoice) {
+computerScoreSpan.innerHTML = computerScore;
+userScoreSpan.innerHTML = userScore;
+winner_div.textContent = "Draw!";
+}
 
-    main(); 
+function game(userChoice) {
+const computerChoice = getComputerChoice();
+switch (userChoice + computerChoice){
+    case "RockScissor":
+    case "PaperRock":
+    case "ScissorPaper":
+            win(userChoice, computerChoice);
+            break;
+    case "RockPaper":
+    case "PaperScissor":
+    case "ScissorRock": 
+            lose(userChoice, computerChoice);
+            break;
+    case "RockRock":
+    case "PaperPaper":
+    case "ScissorScissor":
+            draw(userChoice, computerChoice);
+            break;
+}
+}
+
+function main() {
+ppr_div.addEventListener('click', function() {
+    game("Paper");
+})
+
+rck_div.addEventListener('click', function() {
+    game("Rock");
+})
+
+scssr_div.addEventListener('click', function() {
+    game("Scissor");
+})
+
+}
+
+main();
+
+})

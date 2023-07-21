97c95387936cbe947cdc1f2f8c5dfcd47d050d32
Chris <christopherryan91@gmail.com>
2023-07-21 10:13:35 +0100

    fix css media querie for snake mobile

--- assets/js/snake.js
+++ assets/js/snake.js
@@ -40,7 +40,7 @@ function update() {
     context.fillStyle="black";
     context.fillRect(0, 0, board.width, board.height);
 
-    context.fillStyle="	#baffc9";
+    context.fillStyle="white";
     context.fillRect(foodX, foodY, blockSize, blockSize);
 
     if (snakeX == foodX && snakeY == foodY){

628c24c94ce5e02f3e228ed556107da9b251a0c7
Chris <christopherryan91@gmail.com>
2023-09-10 21:28:02 +0100

    Change format of game to include 24 divs within parent div in game html page

---
+++ game.js
@@ -0,0 +1,6 @@
+var card = document.getElementsByClassName(".card")[0];
+
+card.addEventListener("click", function(){
+    card.style.backgroundImage = url("../images/deck_of_cards/clubs_2.svg");
+});
+

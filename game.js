31c87eeab0f60b9802fefe8e975d4dacc0593bc2
Chris <christopherryan91@gmail.com>
2023-10-02 08:47:15 +0100

    Revert to initial conditions of game before changes

--- game.js
+++ game.js
@@ -8,27 +8,20 @@ const cardValues = [
     { id: '6', value: "assets/images/deck_of_cards/8_of_hearts.png" },
     { id: '7', value: "assets/images/deck_of_cards/9_of_hearts.png" },
 ];
-
-
 const duplicatedCardValues = [...cardValues, ...cardValues];
-
-
 let cards = [];
 let flippedCards = [];
 let matchedPairs = 0;
 let isFlipping = false;
-
 function shuffle(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
     }
 }
-
 function createGameBoard() {
     const gameContainer = document.getElementById('game-container');
     shuffle(duplicatedCardValues);
-
     for (const cardValueObj of duplicatedCardValues) {
         const card = document.createElement('game-container');
         card.className = 'card';
